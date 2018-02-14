319ffcf358f12ec9a12d8b6530b7474719f787e6
roachmanza <roche.olivier@outlook.com>
2018-02-14 15:18:14 +0200

    updated menu and created clipboard

---
+++ clipboard-demo/src/main.js
@@ -0,0 +1,83 @@
+const electron = require('electron')
+const app = electron.app
+const path = require('path')
+const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
+
+const Tray = electron.Tray
+let mainWindow
+
+
+
+app.on('ready', _ => {
+    const appName = electron.app.getName()
+
+    const tray = new Tray(path.join('src', 'trayicon.png'))
+    tray.setToolTip('Tooltip for the context menu')
+    tray.setContextMenu(contextMenu())
+
+
+
+    console.log('ready')
+    mainWindow = new BrowserWindow({
+        height: 400,
+        width: 400
+    })
+
+    mainWindow.on('closed', _ => {
+        console.log('closed')
+        mainWindow = null; //make sure its garbage collected
+    })
+
+    const menu = Menu.buildFromTemplate(appMenu(appName))
+    Menu.setApplicationMenu(menu)
+})
+
+
+function appMenu(appName){
+    const template = [
+        {
+            label: appName,
+            submenu: [{
+                label: "about " + appName,
+                click: _ => {
+                    console.log('clicked')
+                },
+                role: 'about'
+            }, {
+                type: 'separator'
+            }, {
+                label: "quit ",
+                click: _ => {
+                    app.quit()
+                },
+                accelerator: 'Cmd+Q'
+            }]
+        }
+    ]
+    return template;
+}
+
+function contextMenu(){
+    const ctxMenu = new Menu.buildFromTemplate([
+        {
+            label: 'About',
+            click: _ => {
+                console.log('About')
+            }
+        },
+        {
+            label: 'Other',
+            click: _ => {
+                console.log('Other')
+            }
+        },
+        {
+            label: 'Quit',
+            click: _ => {
+                app.quit()
+            }
+        }
+    ])
+    return ctxMenu
+}
